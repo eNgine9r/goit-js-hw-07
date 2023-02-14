a7829de21c80d54e1d755459dc29982c89707e38
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-02-14 22:22:49 +0200

    Update task 1 (in progres)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+
+
+const gallery = document.querySelector(".gallery");
+const imagesMarkup = createImageCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+function createImageCardsMarkup(images) {
+	return images.map(({ preview, original, description }) => {
+		return `
+		<div class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+					/>
+				</a>
+			</div>
+			`;
+	}).join('');
+}
+
